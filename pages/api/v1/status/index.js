9bbbcd44cebf98fb37d7c666b4cd92d07826f67d
Lucas Bobato <lucasbobato88@gmail.com>
2025-06-25 07:53:24 -0300

    Add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,1 +1,9 @@
-<h1>Status API</h1>
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 2 as soma;');
+  console.log(result.rows);
+  response.status(200).json({chave:"são acima da média"});
+}
+
+export default status;
